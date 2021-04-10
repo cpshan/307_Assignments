7cb0ae733e9917b52bc3d6a2fbe0d91592aa0a3e
Cody Shanahan <cody.33shan@gmail.com>
2021-04-10 11:44:13 -0700

    Added axios with CORS

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react';
 import Table from './Table'
 import Form from './form'
+import axois from 'axios';
 
 function MyApp() { 
 	const [characters, setCharacters] = useState([]);
+	const axios = require('axios');
 
 function removeOneCharacter (index) {
 	const updated = characters.filter((character, i) => {
@@ -14,6 +16,25 @@ function removeOneCharacter (index) {
 function updateList(person) {
 	setCharacters([...characters, person]);
 }
+async function fetchAll(){
+	try {
+		const response = await axios.get('http://localhost:5000/users');
+		return response.data.users_list;
+	}
+	catch(error){
+		//Not handling errors. Just logging into console.
+		console.log(error);
+		return false;
+	}
+}
+	
+	useEffect(() => {
+		fetchAll().then(result => {
+			if (result)
+				setCharacters(result);
+		});
+	}, []);
+
 	return (
 		<div className="container">
 			<Table characterData={characters} removeCharacter={removeOneCharacter} />
